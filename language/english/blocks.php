e49b92a433a085fd96f1722777a912a2ec929a09
René Sato <sato-san@impresscms.org>
2011-12-01 12:27:02 +0000

    Make the message boxes better visible and use the standard CSS style from the theme.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/profile/trunk@23032 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/english/blocks.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Extended User Profile
+ *
+ * @copyright	The ImpressCMS Project http://www.impresscms.org/
+ * @license		LICENSE.txt
+ * @license		GNU General Public License (GPL) http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @package		modules
+ * @since		1.2
+ * @version		$Id$
+ */
+
+define("_MB_PROFILE_NUMBER_FRIENDS", "Number of friends to display");
+?>
